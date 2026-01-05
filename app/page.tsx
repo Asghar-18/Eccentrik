20fb2aea3febc496e680223bd974af611da20660
Asghar-18 <Asgharali23890@gmail.com>
2026-01-06 02:21:30 +0500

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,8 @@
 "use client";
-
-import { useState, useEffect } from "react";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-import ProductCard from "../components/product-card";
-import { getFeaturedProducts, Product } from "@/lib/constants/product-data";
 
 export default function Home() {
-  const [featuredProducts, setFeaturedProducts] = useState<Product[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    try {
-      const products = getFeaturedProducts();
-      setFeaturedProducts(products);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error fetching featured products:", error);
-      setFeaturedProducts([]);
-      setIsLoading(false);
-    }
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col">
       <section className="relative h-[100vh] w-full overflow-hidden">
@@ -81,41 +61,6 @@ export default function Home() {
           </Link>
         </div>
       </section>
-
-      {/* <section className="py-16">
-        <div className="mx-auto px-8">
-          <div className="mb-10 flex items-center justify-between">
-            <h2 className="text-2xl sm:text-3xl font-semibold">
-              Featured Collection
-            </h2>
-            <Link
-              href="/new-arrivals"
-              className="flex items-center text-sm font-medium hover:underline"
-            >
-              View All <ArrowRight className="ml-1 h-4 w-4" />
-            </Link>
-          </div>
-
-          {isLoading ? (
-            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-              {[...Array(4)].map((_, i) => (
-                <div key={i} className="animate-pulse">
-                  <div className="mb-4 h-[400px] rounded-lg bg-neutral-200"></div>
-                  <div className="mb-2 h-4 w-1/3 rounded bg-neutral-200"></div>
-                  <div className="mb-2 h-5 w-2/3 rounded bg-neutral-200"></div>
-                  <div className="h-4 w-1/4 rounded bg-neutral-200"></div>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-              {featuredProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section> */}
     </main>
   );
 }
